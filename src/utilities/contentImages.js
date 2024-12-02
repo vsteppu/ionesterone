65847b0ac9f4a0edd0fd01d63a682121c4c062f4
Your Name <vlad@wonsulting.com>
2024-12-02 19:42:44 +0200

    v0.0.1

---
+++ src/utilities/contentImages.js
@@ -0,0 +1,9 @@
+import img1 from '@/media/set1/1.jpg'
+import img2 from '@/media/set2/2.jpg'
+import img3 from '@/media/set3/3.jpg'
+
+export const contentImages = {
+    0:img1,
+    1:img2,
+    2:img3
+}
